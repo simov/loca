50f9394623475087d6357a3336db632db86a96e8
simov <simeonvelichkov@gmail.com>
2014-06-26 21:50:12 +0300

    Node webkit browser context

---
+++ nw/context/test/index.js
@@ -0,0 +1,17 @@
+
+// var $ = window.$;
+
+describe('test suite', function () {
+    it('pass', function (done) {
+        '2'.should.equal('2');
+        console.log($('body'));
+        done();
+    });
+    it('error', function (done) {
+        '3'.should.equal('2');
+        done();
+    });
+    it.skip('skip', function (done) {
+        done();
+    });
+});
